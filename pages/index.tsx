a0b17247877712aeb6b58733c864dcf75239417c
unknown <wasim.grintech@gmail.com>
2023-01-31 10:19:04 +0530

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function IndexPage() {
+  return (
+    <div>
+      Hello World i am God. <Link href="/about">Aboufghdrhrehet</Link>
+    </div>
+  )
+}
